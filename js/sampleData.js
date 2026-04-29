4cece3010d7ed32ca29c6b58684ba46d5a3ca516
Pabitra Ranjan Hota <pabitraranjanhota@gmail.com>
2026-04-29 09:36:12 +0530

    Add sample user data for testing

--- js/sampleData.js
+++ js/sampleData.js
@@ -4,5 +4,13 @@ const SAMPLE_USERS = [
   {
     email: "test@gmail.com",
     password: "123456"
+  },
+  {
+    email: "pabitra@gmail.com",
+    password: "pabi123"
+  },
+   {
+    email: "krishna@gmail.com",
+    password: "krishna"
   }
 ];
